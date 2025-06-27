08a0638ac5f5551c4e5798ecebf313d7d14d2677
ررر نننمح <43173720-realromnc109@users.noreply.replit.com>
2025-06-27 15:28:02 +0000

    Added multiple AFK bots

---
+++ bot1.js
@@ -0,0 +1,10 @@
+const mineflayer = require('mineflayer')
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'SurvivorTown.aternos.me',
+    port: 28617,
+    username: 'AFK_BOT_1'
+  })
+  // باقي الكود نفسه
+}
+createBot()
