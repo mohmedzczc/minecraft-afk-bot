978ed94578412987c9ef61e4e89d3c475de1e6e7
ررر نننمح <43173720-realromnc109@users.noreply.replit.com>
2025-06-27 15:06:15 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// afkBot.js
+const mineflayer = require('mineflayer')
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'Blacko7.aternos.me', // حط هنا عنوان السيرفر بتاعك
+    port: 64385,
+    username: 'AFK_BOT'
+  })
+
+  bot.on('spawn', () => {
+    console.log('Bot spawned and is AFK now.')
+    bot.setControlState('jump', true) // يتحرك كل شوية
+    setInterval(() => {
+      bot.chat('/ping') // لو حبيت تبين إنه شغال
+    }, 60000)
+  })
+
+  bot.on('end', () => {
+    console.log('Bot was kicked, reconnecting...')
+    setTimeout(createBot, 5000) // يرجع تاني لو اترفد
+  })
+}
+
+createBot()
